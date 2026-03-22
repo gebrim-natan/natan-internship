3d29c84a8bea4eec8fb8cd8cb36fc7db2213792d
Natan <natangrvieira@gmail.com>
2026-03-22 20:39:59 -0300

    Implement skeleton loading and AOS scroll animations

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -7,9 +7,13 @@ const LandingIntro = () => {
         <div className="row">
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_wallet"></i>
-              <div className="text">
-                <h4 className="">Set up your wallet</h4>
+              <i
+                className="bg-color-2 i-boxed icon_wallet"
+                data-aos="fade-in"
+                data-aos-delay="50"
+              ></i>
+              <div data-aos="fade-in" data-aos-delay="50" className="text">
+                <h4>Set up your wallet</h4>
                 <p>
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
@@ -18,11 +22,16 @@ const LandingIntro = () => {
               <i className="wm icon_wallet"></i>
             </div>
           </div>
+
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
-              <div className="text">
-                <h4 className="">Add your NFT's</h4>
+              <i
+                className="bg-color-2 i-boxed icon_cloud-upload_alt"
+                data-aos="fade-in"
+                data-aos-delay="50"
+              ></i>
+              <div data-aos="fade-in" data-aos-delay="50" className="text">
+                <h4>Add your NFT&apos;s</h4>
                 <p>
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
@@ -31,11 +40,16 @@ const LandingIntro = () => {
               <i className="wm icon_cloud-upload_alt"></i>
             </div>
           </div>
+
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_tags_alt"></i>
-              <div className="text">
-                <h4 className="">Sell your NFT's</h4>
+              <i
+                className="bg-color-2 i-boxed icon_tags_alt"
+                data-aos="fade-in"
+                data-aos-delay="50"
+              ></i>
+              <div data-aos="fade-in" data-aos-delay="50" className="text">
+                <h4>Sell your NFT&apos;s</h4>
                 <p>
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
